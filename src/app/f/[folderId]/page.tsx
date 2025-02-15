3ed5569b0945d5f44a375ef191510297d315b2c9
Farex <farki.kadric@gmail.com>
2025-02-15 10:24:43 +0100

    data access clean up

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,32 +1,5 @@
-import { db } from "~/server/db"
-import { files as filesSchema, folders as foldersSchema } from "~/server/db/schema"
 import DriveContents from '../../drive-contents'
-import { eq } from "drizzle-orm"
-
-async function getAllParents(folderId : number) {
-    const parents = [];
-    let currentId: number | null = folderId;
-    const visited = new Set<number>(); // Track visited folder IDs
-
-    while (currentId !== null) {
-        if (visited.has(currentId)) {
-            throw new Error("Circular reference detected in folder structure");
-        }
-        visited.add(currentId);
-
-        const folder = await db
-        .select()
-        .from(foldersSchema)
-        .where(eq(foldersSchema.id, currentId));
-
-        if (!folder[0]) {
-            continue
-        }
-        parents.unshift(folder[0]);
-        currentId = folder[0]?.parent;
-    }
-    return parents;
-}
+import { QUERIES } from "~/server/db/queries";
 
 export default async function GoogleDriveClone(props: {
     params: Promise<{folderId: string}>
@@ -42,10 +15,10 @@ export default async function GoogleDriveClone(props: {
 
 
 
-  const filesPromise = db.select().from(filesSchema).where(eq(filesSchema.parent, parsedFolderId))
-  const foldersPromise = db.select().from(foldersSchema).where(eq(foldersSchema.parent, parsedFolderId))
+  const filesPromise = QUERIES.getFiles(parsedFolderId)
+  const foldersPromise = QUERIES.getFolders(parsedFolderId)
 
-  const parentsPromise = getAllParents(parsedFolderId)
+  const parentsPromise = QUERIES.getAllParentsForFolder(parsedFolderId)
 
   const [folders, files, parents] = await Promise.all([foldersPromise, filesPromise, parentsPromise])
 
---
+++ src/server/db/queries.ts
@@ -0,0 +1,55 @@
+import "server-only"
+
+import { db } from "~/server/db"
+import { files_table as filesSchema, folders_table as foldersSchema } from "~/server/db/schema"
+import { eq } from "drizzle-orm"
+
+export const QUERIES = {
+    getFiles : function(folderId: number) {
+
+        return db.select().from(filesSchema).where(eq(filesSchema.parent, folderId))
+    
+    },
+
+    getFolders : function(folderId: number) {
+    
+        return db.select().from(foldersSchema).where(eq(foldersSchema.parent, folderId))
+        
+    },
+
+    getAllParentsForFolder: async function(folderId : number) {
+        const parents = [];
+        let currentId: number | null = folderId;
+        const visited = new Set<number>();
+    
+        while (currentId !== null) {
+            if (visited.has(currentId)) {
+                throw new Error("Circular reference detected in folder structure");
+            }
+            visited.add(currentId);
+    
+            const folder = await db
+            .select()
+            .from(foldersSchema)
+            .where(eq(foldersSchema.id, currentId));
+    
+            if (!folder[0]) {
+                continue
+            }
+            parents.unshift(folder[0]);
+            currentId = folder[0]?.parent;
+        }
+        return parents;
+    }
+
+
+}
+
+
+
+
+
+
+  
+
+
